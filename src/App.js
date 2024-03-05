f608dcd27a61429c55561979ca1f036d314ed1b9
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2024-03-05 17:22:37 +0400

    add todoApp without styles

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FuncTodo from './Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <FuncTodo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todo.js
@@ -0,0 +1,114 @@
+import React, {useCallback, useState} from "react"
+
+const TodoItem = React.memo(({task, onDelete, onDone, id})=> {
+    console.log("re-render")
+    return(
+        <li>
+            {task}
+            <button onClick={() => onDelete(id)}>Delete</button>
+            <button onClick={()=> onDone(task)}>Done</button>
+        </li>
+    )
+})
+
+const DoneTask = React.memo(({task, onDelete, onReset, id})=>{
+    console.log("re-rendered done")
+    return(
+        <li>
+            {task}
+            <button onClick={() => onDelete(id)}>Delete</button>
+            <button onClick={()=> onReset(id)}>Reset</button>
+        </li>
+    )
+})
+
+const FuncTodo = (e) => {
+    const [tasks, setTasks] = useState([]);
+    const [currentTask, setCurrentTask] = useState('');
+    const [doneTasks, setDoneTasks] = useState([]);
+
+    const handleChange = useCallback((event) => {
+        setCurrentTask(event.target.value)
+    },[])
+
+    const handleSubmit = useCallback((event)=>{
+        event.preventDefault()
+        console.log('submit')
+        const task =[...tasks, currentTask]
+        setTasks(task)
+        setCurrentTask('')
+    },[currentTask, tasks]) //44
+
+    const handleDelete = useCallback((id) => {
+        console.log("delete")
+        const updatedTasks = tasks.filter((_, index) => index !== id ) 
+        setTasks(updatedTasks)
+    }, [tasks])
+
+    const handleDone = useCallback((task)=> {
+        console.log("done")
+        setTasks((tasks) => tasks.filter((t) => t !== task))
+        setDoneTasks((dones) => [...dones, task])
+    },[]) //71
+
+    const handleDoneDelete = useCallback((id) => {
+        console.log("Done delete")
+        const updatedTasks= doneTasks.filter((_, index) => index !== id)
+    },[doneTasks])
+
+    const handleReset= useCallback((id) => {
+        console.log("reset")
+        const resetTask = doneTasks[id]
+        const updatedTasks = [...tasks, resetTask]
+        const updatedDoneTasks = doneTasks.filter((_,index) => index !==id)
+        setTasks(updatedTasks)
+        setDoneTasks(updatedDoneTasks)
+    }, [doneTasks, tasks]) //99
+
+    return(
+        <div>
+            <h1>Todo App</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    placeholder="enter a task"
+                    onChange={handleChange}
+                    value={currentTask}
+                />
+                <button type="submit">Add Task</button>
+            </form>
+
+            <ul>
+                {
+                    tasks.map((task, index) => (
+                        <TodoItem 
+                            key={index}
+                            id={index}
+                            task={task}
+                            onDelete={handleDelete}
+                            onDone ={handleDone}
+                        />
+                    ))
+                }
+            </ul>
+
+            <ul>
+                {
+                    doneTasks.map((task, index) => (
+                        <DoneTask 
+                            key={index}
+                            id={index}
+                            task={task}
+                            onDelete={handleDelete}
+                            onReset ={handleReset}
+                        />
+                    ))
+                }
+            </ul>
+        </div>
+    )
+
+}
+
+export default FuncTodo
